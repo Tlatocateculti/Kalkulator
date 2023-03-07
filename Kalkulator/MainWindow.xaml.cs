15475f482f488c33fa4a632f70cf524203744542
Sezmek <97065268+Sezmek@users.noreply.github.com>
2023-03-07 01:53:06 +0100

    essa?

--- Kalkulator/MainWindow.xaml.cs
+++ Kalkulator/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,73 +16,64 @@ using System.Windows.Shapes;
 
 namespace Kalkulator
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
-        double equal;
-        bool operandPressed;
-        string action;
-        List<string> operands;
-        public MainWindow()
+        private ManualResetEvent resetEvent = new ManualResetEvent(false);
+        public string shown;
+        public double hideen;
+        public double savedn;
+        private void btnnumber_Clickend(object sender, RoutedEventArgs e)
         {
-            InitializeComponent();
-            equal = .0;
-            operandPressed = false;
-            action = "";
-            operands = new List<string>();
-            string[] tmp = { "+", "-", "*", "/", "=" };
-            
-            operands.AddRange(tmp.ToList());
-
+            ekran.Text = "";
+            shown += " " + ((Button)sender).Content.ToString() + " ";
+            savedn = hideen;
+            hideen = 0;
+            ekran.Text = shown;
+            switch (((Button)sender).Content.ToString())
+            {
+                case "+":
+                    Task.Run(() =>
+                    {
+                        resetEvent.Reset();
+                        resetEvent.WaitOne();
+                        Dispatcher.Invoke(() =>
+                        {
+                            hideen += savedn;
+                            
+                        });
+                    });
+                    break;
+                case "-":
+                    resetEvent.Reset();
+                    resetEvent.WaitOne(); 
+                    break;
+                case "*":
+                    resetEvent.Reset();
+                    resetEvent.WaitOne(); 
+                    break;
+                case "/":
+                    resetEvent.Reset();
+                    resetEvent.WaitOne(); 
+                    break;
+            }
         }
+
 
         private void btnnumber_Click(object sender, RoutedEventArgs e)
         {
-            var data = ((Button)sender).Content.ToString();
-            if (operandPressed && !operands.Contains(data))
-            {
-                operandPressed = false;
-                ekran.Text = data;
-                return;
-            }
-            if (operands.Contains(data))
-            {
-                switch (action)
-                {
-                    case "+": equal += Convert.ToDouble(ekran.Text); break;
-                    case "-": equal -= Convert.ToDouble(ekran.Text); break;
-                    case "*": equal *= Convert.ToDouble(ekran.Text); break;
-                    case "/": equal /= Convert.ToDouble(ekran.Text); break;
-                    default: equal = Convert.ToDouble(ekran.Text); break;
-                }
-                if (data != "=") action = data;
-
-                ekran.Text = equal.ToString();
-                operandPressed = true;
-            }
-            else
-            {
-                ekran.Text += data.ToString();
-            }
+            ekran.Text = "";
+            shown += ((Button)sender).Content.ToString();
+            hideen += double.Parse(((Button)sender).Content.ToString());
+            ekran.Text += shown;
+            resetEvent.Set();
         }
 
-        private void OK_Button_KeyUp(object sender, KeyEventArgs e)
+        private void btnnumber_Clickended(object sender, RoutedEventArgs e)
         {
-            if (e.Key == Key.C)
-            {
-                ((Button)sender).Content = "WITAJ";
-            }
-            if (e.Key == Key.D)
-            {
-                ((Button)sender).Content = "ŻEGNAJ";
-            }
-        }
-
-        private void etykieta1_KeyDown(object sender, KeyEventArgs e)
-        {
-            //if (ekran.Text)
+            ekran_Copy.Text = ekran.Text;
+            ekran.Text = hideen.ToString();
+            shown = hideen.ToString();
         }
     }
 }
